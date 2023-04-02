df7a507d697e660130d8073810d75719178a2aed
Alex W <amwiederman15@gmail.com>
2023-04-02 11:34:58 -0400

    database works, getting e.split error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -28,7 +28,7 @@ console.log(`Content ${content}`);
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({content});
+  const request = store.add({content: content});
 
   // Get confirmation of the request.
   const result = await request;
